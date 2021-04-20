1a53550c47572e7955d8cb6fa8d83a08a00ba542
Oscar015 <65154615+Oscar015@users.noreply.github.com>
2021-04-20 08:45:01 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -11,29 +11,31 @@ from Delaunay import Delaunay
 
 #puntos = np.asarray([(1,1.9),(3.6,1),(6.5,3), (4,5), (3,5),(1,4),(-1,3)])
 
-puntos = np.asarray([(1,-6), (2.2,-6), (4.5,-3), (5,-1.1), (4.8,1), (3.4,4),\
-                   (1.5,5.1), (-1,5.7),(-3,4.2), (-4.8,2.5), (-6,1.2),\
-                   (-6,-0.7),(-5.8,-2), (-4,-4.5), (-1,-6)])
+puntos = np.asarray([(1,-6), (2.2,-4), (4.5,-3), (5,-1.1), (4.8,1), (3.4,4),
+                    (1.5,5.1), (-1,5.7),(-3,4.2), (-4.8,2.5), (-6,1.2),
+                    (-6,-0.7),(-5.8,-2), (-4,-4.5), (-1,-6)])
+
+# puntos = np.array([(-1,-1,), (-1,1), (1,1), (1,-.5),(0,0)])
 
 td = Delaunay(radius=utils.radiusFrame(puntos))
 
 for punto in puntos:
     td.AddPoint(punto)
-    
-triangulos = td.exportarTriangulos()
-circulos = []#td.exportarCirculos()
 
-utils.PlotDT(puntos,triangulos,circulos, radius=utils.radiusFrame(puntos))
+triangulos = td.exportarTriangulos()
+circulos = td.exportarCirculos()
+
+utils.PlotDT(puntos,triangulos,circulos=[], radius=utils.radiusFrame(puntos))
 area = 0
 for triangulo in triangulos:
     A = puntos[triangulo[0]]
     B = puntos[triangulo[1]]
     C = puntos[triangulo[2]]
-    
+
     area += utils.areaTrianguloDeterminante(A,B,C)
-if(utils.isConvex(puntos)):  
+if(utils.isConvex(puntos)):
     print("El area el poligono es",round(area,3))
-    
-else:
-    print("Esta distribución de puntos no corresponde a un poligono convexo.\n \
-          No es posible calcular el area del poligono.")
+
+# else:
+#     print("Esta distribución de puntos no corresponde a un poligono convexo.\n \
+#           No es posible calcular el area del poligono.")
